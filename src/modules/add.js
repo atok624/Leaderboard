bb08c66d1534647023ed8375e5930860da7ab043
atok624 <84607674+atok624@users.noreply.github.com>
2023-03-19 14:41:25 +0000

    Style the project

--- src/modules/add.js
+++ src/modules/add.js
@@ -16,20 +16,3 @@ const addData = async (player, score) => {
 };
 
 export default addData;
-
-const getID = async () => {
-  const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        name: 'My Game',
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  const myId = await res.json();
-  return myId;
-};
-
-getID();
